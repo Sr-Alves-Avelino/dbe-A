6facc09aad0e3d40c2902c54da9ecc279ac8be47
Joao Carlos <joaocarloslima@me.com>
2021-03-10 19:12:14 -0300

    aula 01

---
+++ loja/src/loja/desconto/TestaDesconto.java
@@ -0,0 +1,16 @@
+package loja.desconto;
+
+import java.math.BigDecimal;
+
+import loja.Pedido;
+
+public class TestaDesconto {
+	public static void main(String[] args) {
+		CalculadoraDeDesconto calculadoraDeDesconto = new CalculadoraDeDesconto();
+		Pedido pedido = new Pedido(new BigDecimal("100"), 9);
+		
+		BigDecimal desconto = calculadoraDeDesconto.calcular(pedido);
+		
+		System.out.println(desconto);
+	}
+}
