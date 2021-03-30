c8aafba3676ba272170a7f5b8bf42cda52d0451d
Joao Carlos <joaocarloslima@me.com>
2021-03-30 13:07:51 -0300

    inicio do projeto progamer

---
+++ progamer/src/main/java/br/com/fiap/beans/SetupBean.java
@@ -0,0 +1,12 @@
+package br.com.fiap.beans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class SetupBean {
+	
+	public void save() {
+		System.out.println("save setup...");
+	}
+
+}
