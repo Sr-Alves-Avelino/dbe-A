d0540c7c310a11155722eba10cf486c92a7cb6ec
João Carlos Lima e Silva <joaocarlos@pop-os.localdomain>
2021-04-06 15:05:18 -0300

    persistencia com JPA

--- progamer/src/main/java/br/com/fiap/beans/SetupBean.java
+++ progamer/src/main/java/br/com/fiap/beans/SetupBean.java
@@ -2,11 +2,26 @@ package br.com.fiap.beans;
 
 import javax.faces.bean.ManagedBean;
 
+import br.com.fiap.dao.SetupDAO;
+import br.com.fiap.model.Setup;
+
 @ManagedBean
 public class SetupBean {
 	
+	private Setup setup = new Setup();
+
 	public void save() {
-		System.out.println("save setup...");
+		new SetupDAO().save(this.setup);
+		System.out.println(this.setup);
 	}
+
+	public Setup getSetup() {
+		return setup;
+	}
+
+	public void setSetup(Setup setup) {
+		this.setup = setup;
+	}
+	
 
 }
